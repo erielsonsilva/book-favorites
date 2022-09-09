86b1b5aa85c8c3dcc085dd79229226748c564d46
erielsonsilva <erielson.silva@yahoo.com.br>
2022-09-08 21:01:45 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Erielson <code>src/App.js</code> and save to reload.
+          Edit Erielson  TESTE <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

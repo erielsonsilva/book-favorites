d2984738e20d6b52778d6607e58113110b455286
erielson <erielson.silva@yahoo.com.br>
2022-09-13 21:08:03 -0300

    rotas routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter, Route ,Routes ,Link } from 'react-router-dom';
+import Login from "./Pages/Login"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Erielson  TESTE <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+  <BrowserRouter>
+    <h1>Book favorites</h1>
+<nav>
+  <ul>
+    <li>
+<Link to='/'>Home</Link>
+    </li>
+    <li>
+      <Link to='/Login'>Login</Link>
+    </li>
+  </ul>
+</nav>
+
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Login" element={<Login />} />
+    <Route path='*'element={<h1>não tenho essa pagina</h1>} />
+  </Routes>
+    </BrowserRouter>
   );
 }
 
